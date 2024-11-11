40301a2a8e8d53f5bd395fd932359d88db89af49
Meriem-BM <barhoumi.meriem1@gmail.com>
2024-11-11 12:37:40 +0100

    refactor: Table component

--- app/components/ui-components/Table.tsx
+++ app/components/ui-components/Table.tsx
@@ -1,49 +1,58 @@
-interface childrenProps {
-  children: React.ReactNode;
+import { FC, ReactNode } from "react";
+
+interface ITableProps {
+  children?: ReactNode;
 }
 
-interface TableCellProps extends childrenProps {
+interface ITableCellProps {
+  children: ReactNode;
   colSpan?: number;
+  className?: string;
 }
 
-const Table = ({ children }: childrenProps) => {
-  return (
-    <div className="w-full flex flex-col">
-      <div className="-m-1.5 overflow-x-auto">
-        <div className="p-1.5 min-w-full inline-block align-middle">
-          <div className="border rounded-lg overflow-hidden">
-            <table className="min-w-full divide-y divide-gray-200">
-              {children}
-            </table>
-          </div>
+export const Table: FC<ITableProps> & {
+  Header: FC<ITableProps>;
+  Body: FC<ITableProps>;
+  Row: FC<ITableProps>;
+  HeadCell: FC<ITableProps>;
+  DataCell: FC<ITableCellProps>;
+} = ({ children }) => (
+  <div className="w-full flex flex-col">
+    <div className="-m-1.5 overflow-x-auto">
+      <div className="p-1.5 min-w-full inline-block align-middle">
+        <div className="border rounded-lg overflow-hidden">
+          <table className="min-w-full divide-y divide-gray-200">
+            {children}
+          </table>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
-const TableHeader = ({ children }: childrenProps) => {
-  return <thead className="bg-gray-50">{children}</thead>;
-};
+Table.Header = ({ children }) => (
+  <thead className="bg-gray-50">{children}</thead>
+);
 
-const TableBody = ({ children }: childrenProps) => {
-  return <tbody className="divide-y divide-gray-200">{children}</tbody>;
-};
+Table.Body = ({ children }) => (
+  <tbody className="divide-y divide-gray-200">{children}</tbody>
+);
 
-const TableRow = ({ children }: childrenProps) => {
-  return <tr>{children}</tr>;
-};
+Table.Row = ({ children }) => <tr>{children}</tr>;
 
-const TableDataCell = ({ children, colSpan }: TableCellProps) => {
-  return (
-    <td colSpan={colSpan} className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
-      {children}
-    </td>
-  );
-};
+Table.HeadCell = ({ children }) => (
+  <th className="text-left px-6 py-3 text-xs font-medium text-gray-500 uppercase">
+    {children}
+  </th>
+);
 
-const TableHeadCell = ({ children }: childrenProps) => {
-  return <th className="text-left px-6 py-3 text-xs font-medium text-gray-500 uppercase">{children}</th>;
-};
-
-export { Table, TableHeader, TableBody, TableRow, TableDataCell, TableHeadCell };
+Table.DataCell = ({ children, colSpan, className }) => (
+  <td
+    colSpan={colSpan}
+    className={`px-6 py-4 whitespace-nowrap text-sm text-gray-800 ${
+      className || ""
+    }`}
+  >
+    {children}
+  </td>
+);
--- app/components/ui-components/index.tsx
+++ app/components/ui-components/index.tsx
@@ -2,26 +2,7 @@ import { Button } from "./Button";
 import { Input } from "./Input";
 import { Checkbox } from "./Checkbox";
 import { SearchBar } from "./SearchBar";
-import {Dropdown} from "./Dropdown";
-import {
-  Table,
-  TableHeader,
-  TableBody,
-  TableRow,
-  TableDataCell,
-  TableHeadCell,
-} from "./Table";
+import { Dropdown } from "./Dropdown";
+import { Table } from "./Table";
 
-export {
-  Button,
-  Input,
-  Checkbox,
-  SearchBar,
-  Dropdown,
-  Table,
-  TableHeader,
-  TableBody,
-  TableRow,
-  TableDataCell,
-  TableHeadCell,
-};
+export { Button, Input, Checkbox, SearchBar, Dropdown, Table };
